24851a89172ed87ef89f6ee0b12bc37bd64dabdd
user <user@LAPTOP-1VG8E5NB>
2021-09-15 11:45:51 +0900

    me-mo

--- Project_WebApp/src/main/resources/memo.sql
+++ Project_WebApp/src/main/resources/memo.sql
@@ -1,6 +1,29 @@
-<script
-  src="https://code.jquery.com/jquery-3.6.0.js"
-  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-  crossorigin="anonymous"></script>
-  
-JQuery Script 문
+/*
+
+1. 제이쿼리 코드
+
+	<script
+	  src="https://code.jquery.com/jquery-3.6.0.js"
+	  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+	  crossorigin="anonymous"></script>
+	  
+	JQuery Script문입니다.
+	AJAX 안정성을 위해 통일 부탁드립니다.
+
+2. 테이블 명세서
+
+	테이블 명세서 이 메모 파일에 작성 부탁드립니다.
+
+3. 깃허브
+
+	
+	항상 작업전에 
+	New Branch -> Pull 
+	이후 작업입니다.
+	
+	커밋시에는
+	add to index -> commit and push -> 
+	깃허브 저장소에서 Pull Request 신청 
+	순서 입니다.
+	
+**/
