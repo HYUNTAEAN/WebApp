925ab570781b4359744f9168f715db9ee325742c
Shin <carbeau@naver.com>
2021-09-27 16:26:22 +0900

    join,login,logout

--- Project_WebApp/src/main/java/net/scit/webapp/service/AppService.java
+++ Project_WebApp/src/main/java/net/scit/webapp/service/AppService.java
@@ -1,9 +1,13 @@
 package net.scit.webapp.service;
 
+import javax.servlet.http.HttpSession;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.scit.webapp.dao.AppRepository;
+import net.scit.webapp.vo.UserVO;
 
 @Service
 public class AppService {
@@ -11,11 +15,60 @@ public class AppService {
 	@Autowired
 	AppRepository repo;
 	
-	
+	@Autowired
+	private HttpSession session;
+
+
 	public void example(){
 		
 	}
 
 
-	
+	public String joinUser(UserVO user) {
+		String path = "";
+		int cnt = repo.joinUser(user);
+		if(cnt > 0) {
+			path = "redirect:/";
+		} else {
+			path = "redirect:/joinForm";
+		}
+		return path;
+	}
+
+
+	public String selectUser(UserVO inputData) {
+		String path = "";
+		UserVO searchData = repo.selectUser(inputData.getUserid());
+		
+		if (searchData == null) {
+			path = "redirect:/loginForm";
+		} else {
+			if (inputData.getUserpwd().equals(searchData.getUserpwd())) {
+				session.setAttribute("loginId", searchData.getUserid());
+				session.setAttribute("loginPwd", searchData.getUserpwd());
+				path = "redirect:/";
+			} else {
+				path = "redirect:/loginForm";
+			}
+		}
+		return path;
+	}
+
+	public String logout() {
+		session.removeAttribute("loginId");
+		return "redirect:/";
+	}
+
+
+	public int userIdCheck(String userid) {
+		UserVO user = repo.userIdCheck(userid);
+		int num = 0;
+		if (user == null) {
+			num = 0;
+		} else {
+			num = 1;
+			
+		}
+		return num;
+	}
 }
